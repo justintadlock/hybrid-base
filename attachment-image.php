5ae1a4204a5a78a13e659c7d605962bae310e628
Justin Tadlock <justin@justintadlock.com>
2013-02-28 21:13:34 -0600

    Remove the calls to functions that are not part of this theme.

--- attachment-image.php
+++ attachment-image.php
@@ -1,58 +1,53 @@
 <?php get_header(); // Loads the header.php template. ?>
 
-	<div id="content">
+	<div id="content" class="hfeed">
 
-		<div class="hfeed">
+		<?php if ( have_posts() ) { ?>
 
-			<?php if ( have_posts() ) : ?>
+			<?php while ( have_posts() ) { ?>
 
-				<?php while ( have_posts() ) : the_post(); ?>
+				<?php the_post(); // Loads the post data. ?>
 
-					<article id="post-<?php the_ID(); ?>" class="<?php hybrid_entry_class(); ?>">
+				<article id="post-<?php the_ID(); ?>" class="<?php hybrid_entry_class(); ?>">
 
-						<header class="entry-header">
-							<?php echo apply_atomic_shortcode( 'entry_title', the_title( '<h1 class="entry-title">', '</h1>', false ) ); ?>
-							<?php echo apply_atomic_shortcode( 'entry_byline', '<div class="entry-byline">' . sprintf( __( 'Sizes: %s', 'hybrid-base' ), chun_get_image_size_links() ) . '</div>' ); ?>
-						</header><!-- .entry-header -->
+					<header class="entry-header">
+						<h1 class="entry-title"><?php single_post_title(); ?></h1>
+					</header><!-- .entry-header -->
 
-						<div class="entry-content">
+					<div class="entry-content">
 
-							<?php if ( has_excerpt() ) {
-								$src = wp_get_attachment_image_src( get_the_ID(), 'full' );
-								echo do_shortcode( sprintf( '[caption align="aligncenter" width="%1$s"]%3$s %2$s[/caption]', esc_attr( $src[1] ), get_the_excerpt(), wp_get_attachment_image( get_the_ID(), 'full', false ) ) );
-							} else {
-								echo wp_get_attachment_image( get_the_ID(), 'full', false, array( 'class' => 'aligncenter' ) );
-							} ?>
+						<?php if ( has_excerpt() ) {
+							$src = wp_get_attachment_image_src( get_the_ID(), 'full' );
+							echo do_shortcode( sprintf( '[caption align="aligncenter" width="%1$s"]%3$s %2$s[/caption]', esc_attr( $src[1] ), get_the_excerpt(), wp_get_attachment_image( get_the_ID(), 'full', false ) ) );
+						} else {
+							echo wp_get_attachment_image( get_the_ID(), 'full', false, array( 'class' => 'aligncenter' ) );
+						} ?>
 
-							<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'hybrid-base' ) ); ?>
-							<?php wp_link_pages( array( 'before' => '<p class="page-links">' . __( 'Pages:', 'hybrid-base' ), 'after' => '</p>' ) ); ?>
+						<?php the_content(); ?>
+						<?php wp_link_pages( array( 'before' => '<p class="page-links">' . __( 'Pages:', 'hybrid-base' ), 'after' => '</p>' ) ); ?>
 
-						</div><!-- .entry-content -->
+					</div><!-- .entry-content -->
 
-					</article><!-- .hentry -->
+				</article><!-- .hentry -->
 
-					<div class="attachment-meta">
+				<div class="attachment-meta">
 
-						<?php chun_image_info(); ?>
+					<?php $gallery = gallery_shortcode( array( 'columns' => 4, 'numberposts' => 8, 'id' => $post->post_parent, 'exclude' => get_the_ID() ) ); ?>
 
-						<?php $gallery = gallery_shortcode( array( 'columns' => 4, 'numberposts' => 8, 'id' => $post->post_parent, 'exclude' => get_the_ID() ) ); ?>
+					<?php if ( !empty( $gallery ) ) { ?>
+						<div class="image-gallery">
+							<h3><?php _e( 'Gallery', 'hybrid-base' ); ?></h3>
+							<?php echo $gallery; ?>
+						</div>
+					<?php } ?>
 
-						<?php if ( !empty( $gallery ) ) { ?>
-							<div class="image-gallery">
-								<h3><?php _e( 'Gallery', 'hybrid-base' ); ?></h3>
-								<?php echo $gallery; ?>
-							</div>
-						<?php } ?>
+				</div><!-- .attachment-meta -->
 
-					</div><!-- .attachment-meta -->
+				<?php comments_template(); // Loads the comments.php template. ?>
 
-					<?php comments_template(); // Loads the comments.php template. ?>
+			<?php } // End while loop. ?>
 
-				<?php endwhile; ?>
-
-			<?php endif; ?>
-
-		</div><!-- .hfeed -->
+		<?php } // End if check. ?>
 
 	</div><!-- #content -->
 
