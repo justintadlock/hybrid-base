cca4edcb69e1bca7f2be8b68816e766c5f0d6535
Justin Tadlock <justin@justintadlock.com>
2014-07-14 17:27:48 -0500

    Switch to new file system. It won't quite work at this point. We need to update a lot of the PHP.

--- comments.php
+++ comments.php
@@ -6,7 +6,19 @@ if ( post_password_required() || ( !have_comments() && !comments_open() && !ping
 
 <section id="comments">
 
-	<?php get_template_part( 'comments-loop' ); // Loads the comments-loop.php template. ?>
+	<?php if ( have_comments() ) { ?>
+
+		<h2 id="comments-number"><?php comments_number( '', __( 'One Response', 'hybrid-base' ), __( '% Responses', 'hybrid-base' ) ); ?></h2>
+
+		<?php get_template_part( 'comments-loop-nav' ); // Loads the comment-loop-nav.php template. ?>
+
+		<ol class="comment-list">
+			<?php wp_list_comments( hybrid_list_comments_args() ); ?>
+		</ol><!-- .comment-list -->
+
+	<?php } // End check for comments. ?>
+
+	<?php get_template_part( 'comments-loop-error' ); // Loads the comments-loop-error.php template. ?>
 
 	<?php comment_form(); // Loads the comment form. ?>
 
--- index.php
+++ index.php
@@ -4,7 +4,27 @@
 
 		<?php get_template_part( 'loop-meta' ); // Loads the loop-meta.php template. ?>
 
-		<?php get_template_part( 'loop' ); // Loads the loop.php template. ?>
+		<?php if ( have_posts() ) { ?>
+
+			<?php while ( have_posts() ) { ?>
+
+				<?php the_post(); // Loads the post data. ?>
+
+				<?php hybrid_get_content_template(); // Loads the content template. ?>
+
+				<?php if ( is_singular() ) { ?>
+
+					<?php comments_template(); // Loads the comments.php template. ?>
+
+				<?php } // End if check. ?>
+
+			<?php } // End while loop. ?>
+
+		<?php } else { ?>
+
+			<?php get_template_part( 'loop-error' ); // Loads the loop-error.php template. ?>
+
+		<?php } // End if check. ?>
 
 		<?php get_template_part( 'loop-nav' ); // Loads the loop-nav.php template. ?>
 
