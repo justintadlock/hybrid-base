006e2ca98508e1a39f1b1862756985905e791c2d
Justin Tadlock <justin@justintadlock.com>
2015-08-18 15:47:40 -0500

    Add <head> attributes.

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html <?php language_attributes( 'html' ); ?>>
 
-<head>
+<head <?php hybrid_attr( 'head' ); ?>>
 <?php wp_head(); // Hook required for scripts, styles, and other <head> items. ?>
 </head>
 
